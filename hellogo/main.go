db62209d7a049d5ef454a73be81e923d771772c6
Gianfranco Reppucci <gianfranco.reppucci@gmail.com>
2014-02-19 02:09:12 +0100

    Introducing hello go package

---
+++ hellogo/main.go
@@ -0,0 +1,11 @@
+package hellogo
+
+import "fmt" 
+
+func SayHello() {
+        fmt.Println("Hello World from hellogo package!")
+}
+
+func yellRandomly() {
+        fmt.Println("Zack Yak Yakketi Yak!")
+}
